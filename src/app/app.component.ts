af426915b14834f1954201650c43ae22d38f3461
Sue Darby <sue.a.darby@gmail.com>
2022-07-15 11:44:09 -0800

    basic html skeleton pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// TODO make this work for the search button in the menu bar
+// const searchButton = document.getElementById('search-button');
+// const searchInput = document.getElementById('search-input');
+// searchButton.addEventListener('click', () => {
+//   const inputValue = searchInput.value;
+//   alert(inputValue);
+// });
 export class AppComponent {
   title = 'Sewing Project Manager';
 }
