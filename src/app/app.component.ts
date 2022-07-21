21118ebac034f37cdb9635506a8a13e55674e093
suedarby <sue.a.darby@gmail.com>
2022-07-20 16:52:53 -0800

    config for Linux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-// TODO make this work for the search button in the menu bar
-// const searchButton = document.getElementById('search-button');
-// const searchInput = document.getElementById('search-input');
-// searchButton.addEventListener('click', () => {
-//   const inputValue = searchInput.value;
-//   alert(inputValue);
-// });
 export class AppComponent {
   title = 'Sewing Project Manager';
 }
-
